c4adccf8d3f5ad547dbb5f6f627b8c09917f1567
aboud20-meet <aboud20@meet.mit.edu>
2018-08-07 18:12:12 +0300

    flask and shit

--- exercises/app.py
+++ exercises/app.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template
 app = Flask(__name__)
-
+like_same_sport = False
 @app.route('/')
 def home_page():
-    pass
+	players = ["Tamer", "mhmd", "aya", "shiraz"]
+	return render_template("index.html", players = players, likes_same_sport = True)
+
+
 
 if __name__ == '__main__':
    app.run(debug = True)
